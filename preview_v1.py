7a275f66f263e37d76c5d93a73c75808d5bbdf81
sQimy <sq1my16@gmail.com>
2021-07-14 11:51:03 +0500

    added

--- preview_v1.py
+++ preview_v1.py
@@ -2,23 +2,24 @@ from re import findall
 import subprocess
 import requests
 
-while True:
+#Получаем ссылку на превью 
+inputLink = input('Youtube url: ')
+pattern = r"=(.*)=?"
+videoID = list(findall(pattern, inputLink))
+previewLink = (f'https://i.ytimg.com/vi/{videoID[0]}/maxresdefault.jpg')
 
-  #Получаем ссылку на превью 
-  inputLink = input('Youtube url: ')
-  pattern = r"=(.*)=?"
-  videoID = list(findall(pattern, inputLink))
-  previewLink = (f'https://i.ytimg.com/vi/{videoID[0]}/maxresdefault.jpg')
+#Сохраняем изображение
+src = requests.get(previewLink)
+with open(str(videoID[0]) + ".jpg", "wb") as image:
+  image.write(src.content)
 
-  #Сохраняем изображение
-  src = requests.get(previewLink)
-  with open(str(videoID[0]) + ".jpg", "wb") as image:
-    image.write(src.content)
+#Открываем папку с изображением
+path = f'D:\Downloads\Preview\{videoID[0]}.jpg'
+print(path)
 
-  #Открываем папку с изображением
-  path = f'D:\Downloaded\preview\{videoID[0]}.jpg'
+#Выделяем файл в проводнике
+subprocess.Popen(f'explorer /select, {path}')
+#subprocess.Popen('exporer /n, /select ' + f'{path}')
 
-  #Выделяем файл в проводнике
-  subprocess.Popen(f'explorer /select, {path}')
-
-  status = input("Success")
+status = input("Success")
+input()
