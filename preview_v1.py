2ce625b31deb357aab900d961fd62bf0cb3828fe
sQimy <sq1my16@gmail.com>
2021-07-15 12:59:36 +0500

    v1.1

--- preview_v1.py
+++ preview_v1.py
@@ -1,24 +1,31 @@
+from os import close
 from re import findall
-import subprocess
+#import subprocess
 import requests
+import os.path, inspect, subprocess
 
+#Ищем где запускаеться файл https://stackoverflow.com/a/44592299
+filename  = inspect.getframeinfo(inspect.currentframe()).filename
+exec_path = os.path.dirname(os.path.abspath(filename))
 
 #Получаем ссылку на превью 
 inputLink = input('Youtube url: ')
-pattern = r"=(.*)?="
+pattern = r"=(.*)"
 videoID = list(findall(pattern, inputLink))
 previewLink = (f'https://i.ytimg.com/vi/{videoID[0]}/maxresdefault.jpg')
 
 #Сохраняем изображение
 src = requests.get(previewLink)
-with open(str(videoID[0]) + ".jpg", "wb") as image:
-  image.write(src.content)
 
-#Открываем папку с изображением
-path = f'D:\Downloads\Preview\{videoID[0]}.jpg'
-print(path)
+fullname = os.path.join(exec_path, videoID[0] + ".jpg")
+image = open(fullname, "wb")
+image.write(src.content)
+image.close()
 
-#Выделяем файл в проводнике
-subprocess.Popen(f'explorer /select, {path}')
+#выделяем файл в проводнике
+subprocess.Popen(f'explorer /select, {fullname}')
 
-status = input("Success")
+status = input("Success ")
+print(videoID)
+print(exec_path)
+print(fullname)
